9ad43e1265d0e12e6742fe091e7d7846d124e1c7
j4rl <charlie.jarl@gmail.com>
2021-02-15 14:46:33 +0100

    Bleh

--- json.php
+++ json.php
@@ -1,5 +1,5 @@
 <?php 
 require_once 'util.php';
 $x=new Database("dbsdata");
-echo $x->data2JSON("SELECT * FROM tbluser WHERE userlevel>15 ORDER BY userlevel DESC");
+echo $x->data2JSON("SELECT * FROM tbluser");
 ?>
--- toJSONex.php
+++ toJSONex.php
@@ -7,28 +7,28 @@ $db="dbsData";
 $conn=mysqli_connect($host,$user,$pass,$db) or die("Couldn't connect!");
 //--------------------------------------------------------------------
 
-// /**
-//  * data2JSON
-//  * Returns data from database formatted as JSON with the object name data
-//  * @param object $connOBJ the database connection object.
-//  * @param string $txtSQL String with SQL-formatted question.
-//  * @return string JSON formatted string
-//  */
-//  function data2JSON($connOBJ, $txtSQL){
-//     $result = mysqli_query($connOBJ, $txtSQL) or die(mysqli_error($conn));
-//     $str= '{"data":[';
-//         while($rad=$result->fetch_assoc()){
-//             $userID=$rad['userID'];
-//             $username=$rad['username'];
-//             $password=$rad['password'];
-//             $userlevel=$rad['userlevel'];
-//             $str=$str.'{"userID":'.$userID.',"username":"'.$username.'","password":"'.$password.'","userlevel":'.$userlevel.'},';
-//         };   
-//         $str=rtrim($str, ",");
-//         $str=$str."]}";
-//         return $str;
-// }
-//echo data2JSON($conn, "SELECT * FROM tbluser");
+ /**
+  * data2JSON
+  * Returns data from database formatted as JSON with the object name data
+  * @param object $connOBJ the database connection object.
+  * @param string $txtSQL String with SQL-formatted question.
+  * @return string JSON formatted string
+  */
+  function data2JSON($connOBJ, $txtSQL){
+     $result = mysqli_query($connOBJ, $txtSQL) or die(mysqli_error($conn));
+     $str= '{"data":[';
+         while($rad=$result->fetch_assoc()){
+             $userID=$rad['userID'];
+             $username=$rad['username'];
+             $password=$rad['password'];
+             $userlevel=$rad['userlevel'];
+             $str=$str.'{"userID":'.$userID.',"username":"'.$username.'","password":"'.$password.'","userlevel":'.$userlevel.'},';
+         };   
+         $str=rtrim($str, ",");
+        $str=$str."]}";
+         return $str;
+ }
+echo data2JSON($conn, "SELECT * FROM tbluser");
 
 
 //--------------------------------Plain QD solution with echo
