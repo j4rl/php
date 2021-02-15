5c337b9b174ca0ab28d3fb2f820a90a875dde0e7
j4rl <charlie.jarl@gmail.com>
2021-02-15 10:35:09 +0100

    moar

--- form.php
+++ form.php
@@ -8,14 +8,14 @@
 </head>
 <body>
 <?php 
-    if(isset($_POST['txtField'])){
+    if(isset($_POST['btnSubmit'])){
         echo "<h1>".$_POST['txtField']."</h1>";
         if(isset($_GET['stuff'])) echo "<h2>".$_GET['stuff']."</h2>";
     } 
 ?>
-    <form name="frmForm" id="frmForm" action="form.php?stuff=Arne" method="POST">
-        <input type="text" name="txtField" id="txtField" placeholder="Skriv vad som helst">
-        <button type="submit" name="btnSubmit" id="btnSubmit">Tryck</button>
+    <form name="frmForm" id="frmForm" action="form.php?stuff=Kim%20Hultgren" method="POST">
+        <input type="text" name="txtField" id="txtField" placeholder="Personnummer 12 siffror" pattern="(19|20)[0-9]{6}-[0-9]{4}" title="yyyymmdd-nnnn">
+        <button type="submit" name="btnSubmit" id="btnSubmit" value="-">Tryck</button>
     </form>
 </body>
 </html>
