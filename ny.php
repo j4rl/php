5abf19e1c68b6ef841a3e843b37b93f74a575599
j4rl <charlie.jarl@gmail.com>
2021-04-06 15:23:46 +0200

    v144.2 small uppdates

--- ny.php
+++ ny.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
 <?php
-    function showText(){
-        if(isset($_POST['btnSubmit'])){ //Har jag tryckt på knappen?
-            return $_POST['txtInput']." ".$_POST['txtHidden'];
-        }else{
-            return "";
-        }    
+    $host="localhost";
+    $user="root";
+    $pass="";
+    $db ="dbsData";
+    $conn=mysqli_connect($host, $user, $pass, $db);
+
+    if(isset($_POST['btnSubmit'])){
+        $txtText=$_POST['txtInput'];
+        $sql="INSERT INTO `tbltext`(`text`) VALUES ('$txtText')";
+        $result=mysqli_query($conn, $sql);
     }
 ?>
 <html>
@@ -15,11 +19,18 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <?=showText();?>
     <form method="post" action="ny.php">
         <input type="text" name="txtInput" id="txtInput" placeholder="Skriv vad du vill!" required autocomplete="off">
-        <input type="hidden" name="txtHidden" value="<?=showText();?>">
         <button type="submit" name="btnSubmit" value=".">Tryck!</button>
     </form>
+    <div class="ruta">
+    <?php 
+        $sql="SELECT * FROM tblText";
+        $result=mysqli_query($conn, $sql);
+        while($row=mysqli_fetch_assoc($result)){
+            echo $row['text']."<br>";
+        }
+    ?>
+    </div>
 </body>
 </html>
