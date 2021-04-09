423eb7d8a0c3f0cb1b930dd9ec79f9441025cabe
j4rl <charlie.jarl@gmail.com>
2021-04-09 11:01:03 +0200

    114.3

--- ny.php
+++ ny.php
@@ -1,16 +1,28 @@
 <!DOCTYPE html>
 <?php
+    //------------------------------Connect to database
     $host="localhost";
     $user="root";
     $pass="";
     $db ="dbsData";
     $conn=mysqli_connect($host, $user, $pass, $db);
+    //------------------------------------------------
 
+    //--------------------------------If FORM is sent, 
+    //---take data from textbox and put it in database
     if(isset($_POST['btnSubmit'])){
         $txtText=$_POST['txtInput'];
         $sql="INSERT INTO `tbltext`(`text`) VALUES ('$txtText')";
         $result=mysqli_query($conn, $sql);
     }
+    //-----------------------------------------------
+    
+    if(isset($_GET['del'])){
+        $delid=$_GET["del"];
+        $sql="DELETE FROM `tbltext` WHERE TextID=$delid";
+        $result=mysqli_query($conn, $sql);
+    }
+
 ?>
 <html>
 <head>
@@ -25,11 +37,13 @@
     </form>
     <div class="ruta">
     <?php 
+        //-------------------Get all data from table, and print it
         $sql="SELECT * FROM tblText";
         $result=mysqli_query($conn, $sql);
         while($row=mysqli_fetch_assoc($result)){
-            echo $row['text']."<br>";
+            echo $row['text']."&nbsp;<a href='ny.php?del=".$row['TextID']."'>X</a><br>";
         }
+        //--------------------------------------------------------
     ?>
     </div>
 </body>
